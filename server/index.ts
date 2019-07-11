9a389309d209573f60f93b52520899b4bb40c1c5
Andrei Sarabia <andrei@easybreathe.com>
2019-07-11 16:10:14 -0700

    potential...

--- server/index.ts
+++ server/index.ts
@@ -3,15 +3,19 @@ require('dotenv').config();
 import Koa from 'koa';
 import koaMount from 'koa-mount';
 import koaSession from 'koa-session';
-import AdminApplication from './private/AdminApplication';
+// import AdminApplication from './private/AdminApplication';
+import AdminRouter from './private/routes/AdminRouter';
 
 const main = async (): Promise<void> => {
-  const adminApp = new AdminApplication();
+  const adminApp = new Koa();
   const app = new Koa();
+  const adminRouter = new AdminRouter();
 
-  app.use(koaMount('/admin', await adminApp.setup(app)));
+  adminApp
+    .use(adminRouter.middleware.routes())
+    .use(adminRouter.middleware.allowedMethods());
 
-  // await adminApp.setup();
+  app.use(koaMount('/admin', adminApp));
 
   app.listen(+process.env.PORT || 3000, () => console.log('Listening...'));
 };
